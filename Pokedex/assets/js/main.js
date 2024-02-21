c25fd9ae2e1f6db91b01479e8dbf4eabe8ca5b1d
muniz94 <muniz91@hotmail.com>
2024-02-20 21:56:22 -0300

    Projeto Pokedex

---
+++ Pokedex/assets/js/main.js
@@ -0,0 +1,60 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li onclick="pokemonDetails(${pokemon.number})" class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+            <dialog class="dialog-${pokemon.number}">
+                <img src="${pokemon.photo}" width="100px" height="100px">
+                <p class="height">Nome: ${pokemon.name}</p>
+                <p class="height">Altura: ${pokemon.height}</p>
+                <p class="weight">Peso: ${pokemon.weight}</p>
+                <p class="type">Tipo: ${pokemon.type}</p>
+            </dialog>
+        </li>
+    `
+}
+
+
+function pokemonDetails(number) {
+    const modal = document.querySelector(`.dialog-${number}`)
+    modal.showModal();
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
---
+++ Pokedex/assets/js/pokemon-model.js
@@ -0,0 +1,14 @@
+
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+}
+
+class PokemonStats {
+    height;
+    weight;
+}
+
